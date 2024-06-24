b63ff10edd23bb7eaf41e8f893f988e26db47cbb
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2024-06-24 18:28:07 +0700

    asgsgga

---
+++ resources/views/customer/carts/carts/index.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Carts') }}
+        </h2>
+        <div class="text-gray-900">
+            {{ __("You're logged in as Student!") }} 
+        </div>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    @if ($cart->isEmpty())
+                        <p class="text-gray-900">Your cart is empty.</p>
+                    @else
+                        <table class="table-auto w-full">
+                            <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                                <tr>
+                                    <th class="p-2">Book</th>
+                                    <th class="p-2">Quantity</th>
+                                    <th class="p-2">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-sm divide-y divide-gray-100">
+                                @foreach ($cart as $cartItem)
+                                    <tr>
+                                        <td class="p-2">{{ $cartItem->book->title }}</td>
+                                        <td class="p-2">{{ $cartItem->quantity }}</td>
+                                        <td class="p-2">
+                                            <form action="{{ route('student.carts.destroy', ['cart' => $cartItem]) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-500 hover:text-red-700">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
